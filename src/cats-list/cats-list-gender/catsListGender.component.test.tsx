b524c4703715625612085122bf3aa51aae0ef3fc
sukhrajsingh89 <54345240+sukhrajsingh89@users.noreply.github.com>
2023-11-13 20:27:02 +1100

    Updated test file

--- src/cats-list/cats-list-gender/catsListGender.component.test.tsx
+++ src/cats-list/cats-list-gender/catsListGender.component.test.tsx
@@ -1,131 +1,171 @@
 import React from "react";
-import {render,cleanup } from "@testing-library/react";
- import CatsListGender from "./catsListGender.component";
+import { render, cleanup } from "@testing-library/react";
+import CatsListGender from "./catsListGender.component";
 
+describe("<CatsList", () => {
+  afterEach(() => {
+    cleanup();
+  });
 
-describe('<CatsList',()=>{
-    
- 
+  const renderComponent = (props = {}) => {
+    return render(<CatsListGender userObject={[]} gender={undefined} />);
+  };
 
-    afterEach(()=>{
-        cleanup()
-    })
-
-const renderComponent = (props={}) =>{
-    return render (<CatsListGender {...props}/>)
-}
-
-it('it should render the component',()=>{
-
-    const props ={
-        userObject:[
-         {
-           "name": "Bob",
-           "gender": "Male",
-           "age": 23,
-           "pets": [{"name": "Garfield", "type": "Cat"}, {"name": "Fido", "type": "Dog"}]
-         },
-         {"name": "Jennifer", "gender": "Female", "age": 18, "pets": [{"name": "Garfield", "type": "Cat"}]},
-         {"name": "Steve", "gender": "Male", "age": 45, "pets": null},
-         {
-           "name": "Fred",
-           "gender": "Male",
-           "age": 40,
-           "pets": [
-             {"name": "Tom", "type": "Cat"},
-             {"name": "Max", "type": "Cat"},
-             {"name": "Sam", "type": "Dog"},
-             {"name": "Jim", "type": "Cat"}
-           ]
-         },
-         {"name": "Samantha", "gender": "Female", "age": 40, "pets": [{"name": "Tabby", "type": "Cat"}]},
-         {
-           "name": "Alice",
-           "gender": "Female",
-           "age": 64,
-           "pets": [{"name": "Simba", "type": "Cat"}, {"name": "Nemo", "type": "Fish"}]
-         }
-       ],
-       gender:'Male'
-     
-      }
-    const {asFragment}=renderComponent(props)
-    expect(asFragment()).toMatchSnapshot()
-})
-
-it('it should render the cat list based on gender Male',()=>{
-
-    const defaultProps ={ userObject:[
+  it("it should render the component", () => {
+    const props = {
+      userObject: [
         {
-          "name": "Bob",
-          "gender": "Male",
-          "age": 23,
-          "pets": [{"name": "Garfield", "type": "Cat"}, {"name": "Fido", "type": "Dog"}]
+          name: "Bob",
+          gender: "Male",
+          age: 23,
+          pets: [
+            { name: "Garfield", type: "Cat" },
+            { name: "Fido", type: "Dog" },
+          ],
         },
-        {"name": "Jennifer", "gender": "Female", "age": 18, "pets": [{"name": "Garfield", "type": "Cat"}]},
-        {"name": "Steve", "gender": "Male", "age": 45, "pets": null},
         {
-          "name": "Fred",
-          "gender": "Male",
-          "age": 40,
-          "pets": [
-            {"name": "Tom", "type": "Cat"},
-            {"name": "Max", "type": "Cat"},
-            {"name": "Sam", "type": "Dog"},
-            {"name": "Jim", "type": "Cat"}
-          ]
+          name: "Jennifer",
+          gender: "Female",
+          age: 18,
+          pets: [{ name: "Garfield", type: "Cat" }],
         },
-        {"name": "Samantha", "gender": "Female", "age": 40, "pets": [{"name": "Tabby", "type": "Cat"}]},
+        { name: "Steve", gender: "Male", age: 45, pets: null },
         {
-          "name": "Alice",
-          "gender": "Female",
-          "age": 64,
-          "pets": [{"name": "Simba", "type": "Cat"}, {"name": "Nemo", "type": "Fish"}]
-        }
+          name: "Fred",
+          gender: "Male",
+          age: 40,
+          pets: [
+            { name: "Tom", type: "Cat" },
+            { name: "Max", type: "Cat" },
+            { name: "Sam", type: "Dog" },
+            { name: "Jim", type: "Cat" },
+          ],
+        },
+        {
+          name: "Samantha",
+          gender: "Female",
+          age: 40,
+          pets: [{ name: "Tabby", type: "Cat" }],
+        },
+        {
+          name: "Alice",
+          gender: "Female",
+          age: 64,
+          pets: [
+            { name: "Simba", type: "Cat" },
+            { name: "Nemo", type: "Fish" },
+          ],
+        },
       ],
-      gender:'Male'}
+      gender: "Male",
+    };
+    const { asFragment } = renderComponent(props);
+    expect(asFragment()).toMatchSnapshot();
+  });
 
-    
-    const {asFragment}=renderComponent(defaultProps)
-    expect(asFragment()).toMatchSnapshot()
-})
-
-
-
-it('it should render the cat list based on gender Female',()=>{
-
-    const defaultProps ={ userObject:[
+  it("it should render the cat list based on gender Male", () => {
+    const defaultProps = {
+      userObject: [
         {
-          "name": "Bob",
-          "gender": "Male",
-          "age": 23,
-          "pets": [{"name": "Garfield", "type": "Cat"}, {"name": "Fido", "type": "Dog"}]
+          name: "Bob",
+          gender: "Male",
+          age: 23,
+          pets: [
+            { name: "Garfield", type: "Cat" },
+            { name: "Fido", type: "Dog" },
+          ],
         },
-        {"name": "Jennifer", "gender": "Female", "age": 18, "pets": [{"name": "Garfield", "type": "Cat"}]},
-        {"name": "Steve", "gender": "Male", "age": 45, "pets": null},
         {
-          "name": "Fred",
-          "gender": "Male",
-          "age": 40,
-          "pets": [
-            {"name": "Tom", "type": "Cat"},
-            {"name": "Max", "type": "Cat"},
-            {"name": "Sam", "type": "Dog"},
-            {"name": "Jim", "type": "Cat"}
-          ]
+          name: "Jennifer",
+          gender: "Female",
+          age: 18,
+          pets: [{ name: "Garfield", type: "Cat" }],
         },
-        {"name": "Samantha", "gender": "Female", "age": 40, "pets": [{"name": "Tabby", "type": "Cat"}]},
+        { name: "Steve", gender: "Male", age: 45, pets: null },
         {
-          "name": "Alice",
-          "gender": "Female",
-          "age": 64,
-          "pets": [{"name": "Simba", "type": "Cat"}, {"name": "Nemo", "type": "Fish"}]
-        }
+          name: "Fred",
+          gender: "Male",
+          age: 40,
+          pets: [
+            { name: "Tom", type: "Cat" },
+            { name: "Max", type: "Cat" },
+            { name: "Sam", type: "Dog" },
+            { name: "Jim", type: "Cat" },
+          ],
+        },
+        {
+          name: "Samantha",
+          gender: "Female",
+          age: 40,
+          pets: [{ name: "Tabby", type: "Cat" }],
+        },
+        {
+          name: "Alice",
+          gender: "Female",
+          age: 64,
+          pets: [
+            { name: "Simba", type: "Cat" },
+            { name: "Nemo", type: "Fish" },
+          ],
+        },
       ],
-      gender:'Female'}
+      gender: "Male",
+    };
 
-    const {asFragment}=renderComponent(defaultProps)
-    expect(asFragment()).toMatchSnapshot()
-})
+    const { asFragment } = renderComponent(defaultProps);
+    expect(asFragment()).toMatchSnapshot();
+  });
 
-})
+  it("it should render the cat list based on gender Female", () => {
+    const defaultProps = {
+      userObject: [
+        {
+          name: "Bob",
+          gender: "Male",
+          age: 23,
+          pets: [
+            { name: "Garfield", type: "Cat" },
+            { name: "Fido", type: "Dog" },
+          ],
+        },
+        {
+          name: "Jennifer",
+          gender: "Female",
+          age: 18,
+          pets: [{ name: "Garfield", type: "Cat" }],
+        },
+        { name: "Steve", gender: "Male", age: 45, pets: null },
+        {
+          name: "Fred",
+          gender: "Male",
+          age: 40,
+          pets: [
+            { name: "Tom", type: "Cat" },
+            { name: "Max", type: "Cat" },
+            { name: "Sam", type: "Dog" },
+            { name: "Jim", type: "Cat" },
+          ],
+        },
+        {
+          name: "Samantha",
+          gender: "Female",
+          age: 40,
+          pets: [{ name: "Tabby", type: "Cat" }],
+        },
+        {
+          name: "Alice",
+          gender: "Female",
+          age: 64,
+          pets: [
+            { name: "Simba", type: "Cat" },
+            { name: "Nemo", type: "Fish" },
+          ],
+        },
+      ],
+      gender: "Female",
+    };
+
+    const { asFragment } = renderComponent(defaultProps);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
