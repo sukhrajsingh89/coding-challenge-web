9fcfe60159dab2463775e8aa10be5cf2369f3f2e
sukhrajsingh89 <54345240+sukhrajsingh89@users.noreply.github.com>
2023-11-14 12:00:13 +1100

    Implemented try catch

--- src/components/cats-list/catsList.component.tsx
+++ src/components/cats-list/catsList.component.tsx
@@ -1,39 +1,38 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Spacer ,DivWrapper} from "./catsList.style";
+import { Spacer, DivWrapper } from "./catsList.style";
 import { IUsers } from "./types";
-import CatsListGender from './cats-list-gender/catsListGender.component'
+import CatsListGender from "./cats-list-gender/catsListGender.component";
 
 import { REACT_APP_API_URL } from "./catsList.utils";
-
 
 const CatsList = () => {
   const [users, setUsers] = useState<IUsers[]>([]);
 
-
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get(
-        REACT_APP_API_URL
-      );
+    try {
+      const fetchData = async () => {
+        const response = await axios.get(REACT_APP_API_URL);
 
-      setUsers(response.data);
-    };
+        setUsers(response.data);
+      };
 
-    fetchData();
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
   }, []);
 
   return (
     <>
       <DivWrapper>
-      <div data-testid="male-list">Male</div>
-      <Spacer />
-      <CatsListGender userObject={users} gender={"Male"}/>
-       <Spacer />
-      <div data-testid ="female-list">Female</div>
-      <Spacer />
-      <CatsListGender userObject={users} gender={"Female"}/>
-     
+        <div data-testid="male-list">Male</div>
+        <Spacer />
+        <CatsListGender userObject={users} gender={"Male"} />
+        <Spacer />
+        <div data-testid="female-list">Female</div>
+        <Spacer />
+        <CatsListGender userObject={users} gender={"Female"} />
       </DivWrapper>
     </>
   );
