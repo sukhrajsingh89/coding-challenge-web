e2a45ae18f040e1bfbf3797baeded0e39134e050
sukhrajsingh89 <54345240+sukhrajsingh89@users.noreply.github.com>
2023-11-14 15:53:48 +1100

    more changes

--- src/components/cats-list/catsList.utils.ts
+++ src/components/cats-list/catsList.utils.ts
@@ -16,7 +16,7 @@ export const catsListFilter = (list: IUsers[],gender:string): ICats[] => {
 };
 
 
-// Below has been used because .env is not working in my local .There seems to have some issue with webpack 5
+
 
 export const REACT_APP_API_URL= "https://gist.githubusercontent.com/medibank-digital/a1fc81a93200a7b9d5f8b7eae0fac6f8/raw/de10a4fcf717e6c431e88c965072c784808fd6b2/people.json"
 
